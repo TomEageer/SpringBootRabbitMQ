3eaea695367bafcf8f5c0bc382d5d68c8f9a395d
唐艺家 <1245012809@qq.com>
2023-07-14 14:15:26 +0800

    测试

--- src/main/java/com/xdclass/xdclasssp/config/RabbitMQConfig.java
+++ src/main/java/com/xdclass/xdclasssp/config/RabbitMQConfig.java
@@ -1,8 +1,7 @@
 package com.xdclass.xdclasssp.config;
 
 import com.rabbitmq.client.AMQP;
-import org.springframework.amqp.core.Exchange;
-import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,9 +12,33 @@ public class RabbitMQConfig {
 
     public static final String QUEUE = "order_queue";
 
+    /**
+     * topic交换机
+     * @return
+     */
     @Bean
     public Exchange orderExchange(){
         return ExchangeBuilder.topicExchange(EXCHANGE_NAME).durable(true).build();
+    }
+
+    /**
+     * 队列
+     * @return
+     */
+    @Bean
+    public Queue orderQueue(){
+        return QueueBuilder.durable(QUEUE).build();
+    }
+
+    /**
+     * 交换机和队列绑定关系
+     * @param queue
+     * @param exchange
+     * @return
+     */
+    @Bean
+    public Binding orderBinding(Queue queue, Exchange exchange){
+        return BindingBuilder.bind(queue).to(exchange).with("order.#").noargs();
     }
 
 }
--- src/test/java/com/xdclass/xdclasssp/XdclassSpApplicationTests.java
+++ src/test/java/com/xdclass/xdclasssp/XdclassSpApplicationTests.java
@@ -1,13 +1,20 @@
 package com.xdclass.xdclasssp;
 
+import com.xdclass.xdclasssp.config.RabbitMQConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class XdclassSpApplicationTests {
 
+	@Autowired
+	public RabbitTemplate template;
+
 	@Test
-	void contextLoads() {
+	void testSend() {
+		template.convertAndSend(RabbitMQConfig.EXCHANGE_NAME, "order.new", "新订单");
 	}
 
 }
